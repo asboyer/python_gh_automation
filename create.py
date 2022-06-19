85b2717f1f4c5202a8e935faeaaada50edac0571
boyer <asboyer@gmail.com>
2022-06-19 16:10:33 -0400

    first commit

---
+++ create.py
@@ -0,0 +1,16 @@
+import sys
+import os
+from github import Github
+
+username = sys.argv[1]
+gh_key = sys.argv[2]
+repo_name = sys.argv[3]
+
+def create():
+    user = Github(gh_key).get_user()
+    repo = user.create_repo(repo_name)
+
+    print(f"Succesfully created repository {repo_name}")
+
+if __name__ == "__main__":
+    create()
